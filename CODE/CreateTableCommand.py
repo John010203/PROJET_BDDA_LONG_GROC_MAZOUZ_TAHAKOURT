183920ff2fa52e8b63d0f55cbe617165d699bd3f
mica1505 <cameliamazouz1505@gmail.com>
2023-11-23 16:53:14 +0100

    CreateTableCommand

--- CODE/CreateTableCommand.py
+++ CODE/CreateTableCommand.py
@@ -1,4 +1,6 @@
 from TableInfo import TableInfo
+from ColInfo import ColInfo 
+
 class CreateTableCommand:
     def __init__(self,nomRelation,nbColonnes,colInfos):
         self.nomRelation=nomRelation
@@ -6,5 +8,28 @@ class CreateTableCommand:
         self.colInfos=colInfos
     
     def Execute(self)->None:
-        TableInfo(self.nomRelation,self.nbColonnes,self.colInfos)
+        """
+        appeler createNewHeaderPage du FileManager
+        • créer une TableInfo en utilisant les données « communes », ainsi que le PageId rendu par
+        l'appel à createHeaderPage ci-dessus
+        • rajouter la TableInfo au DatabaseInfo avec la méthode qui convient
+        """
+        TableInfo(self.nomRelation,self.nbColonnes,self.colInfos)#faut rajouter le headerPage
+        
         return
+    
+
+    #CREATE TABLE NomRelation (NomCol_1:TypeCol_1,NomCol_2:TypeCol_2,NomCol_NbCol:TypeCol_NbCol)
+    def parseCommandeCreateTable(string):
+        args = string[12:len(string)-1]
+        nomRelation = args.split('(')[0]
+        cols = args.split('(')[1].split(',')
+        listCols = []
+
+        for c in cols:
+            nomCol = c.split(':')[0]
+            typeCol = c.split(':')[1]
+            listCols.append(ColInfo(nomCol,typeCol))
+
+        return nomRelation,len(listCols),listCols
+        
---
+++ CODE/ResetDBCommand.py
@@ -0,0 +1,17 @@
+class ResetDBCommand():
+
+    def __init__(self,bdd):
+        self.bdd = bdd
+        self.diskManager = bdd.disk_manager
+        self.bufferManager = bdd.buffer_manager
+        self.databaseInfo = bdd.data_base_info
+
+    def Reset(self):
+        '''
+         supprimer tous les fichiers du dossier DB
+        • « remettre tout à 0 » dans le BufferManager et la DatabaseInfo, ainsi que potentiellement
+        dans le DiskManager.
+        Pour cela, il faut vider les listes, remettre tous les compteurs et les flags à 0, etc.
+        Vous pouvez faire ces « remises à 0 » dans des méthodes spécifiques, à créer sur chaque
+        classe concernée.
+        '''

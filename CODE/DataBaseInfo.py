4318f2e3ed07a8674720b144eb0d4f391e898380
mica1505 <cameliamazouz1505@gmail.com>
2023-11-23 15:07:02 +0100

    ajout de commentaires

--- CODE/DataBaseInfo.py
+++ CODE/DataBaseInfo.py
@@ -3,13 +3,15 @@ import TableInfo as TableInfo
 class DataBaseInfo :
     def __init__(self, db):
         self.db=db
-        self.tableInfo : list = []
+        self.tableInfo : list = [] #toute sles relations de notre BDD
         #pas besoin de compteur size
         #pas besoin d'initialiser un compteur, on retourne jujste la taille du tableau
     def Init() -> None :
+        #recuperer les definitions des tables, ecritent dans un fichier
         return
     
     def Finish() -> None:
+        #ecrire les definitons des tables dans le fichier 
         return
     
     def getNbRelations(self) -> int:
